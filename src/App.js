1f036558c2f5e5ad4a36bf19b2dca7e731688d00
isabelleanno <itsisabelleanno@gmail.com>
2022-06-21 07:17:02 -0400

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
 import "./App.css";
 import "./Search.js";
 import Search from "./Search.js";
 export default function App() {
   return (
     <div className="App">
-      <h1>Fake Weather Search Engine- Deployed on GitHub </h1>
-      <h3>SheCodes Week 4 Homework</h3>
-      <Search />
+      <div className="container">
+        <h1>Weather App </h1>
 
-      <div className="small">
-        <small>
-          Made by Isabelle Anno. For educational uses only. This weather data is
-          fake & randomized.
-        </small>
+        <Search />
+
+        <div className="small">
+          <small>
+            This project was coded by{" "}
+            <a href="https://isabelleanno-portfolio.netlify.app/">
+              Isabelle Anno
+            </a>
+            . Check out my <a href="https://github.com/isabelleanno">GitHub</a>!
+          </small>
+        </div>
       </div>
     </div>
   );
